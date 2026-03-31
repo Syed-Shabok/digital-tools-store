bce109112b10c341f2887307fdea51b7054254de
Syed-Shabok <syed.shabok@gmail.com>
2026-03-31 20:58:47 +0600

    Project Initialized.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-center text-3xl text-blue-500 font-semibold">
+          Project Initialized
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
